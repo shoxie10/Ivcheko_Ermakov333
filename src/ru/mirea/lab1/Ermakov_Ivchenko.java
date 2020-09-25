6479ca9a53391256487c3e728e79ff3e9d3f2114
Студент <student@ippo.mirea.ru>
2020-09-25 15:22:00 +0300

    Initial commit

---
+++ src/ru/mirea/lab1/Ermakov_Ivchenko.java
@@ -0,0 +1,12 @@
+package ru.mirea.lab1;
+import java.lang.*;
+
+public class Ermakov_Ivchenko {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10, "blue");
+        System.out.println(c1);
+
+
+    }
+}
+
